339aad47ab02da2b520cbd9c528e835141cde66b
Mason A. Dean <100883553+Graphing-wiz@users.noreply.github.com>
2022-07-10 20:25:17 -0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,4 @@
+console.log("this file was grabbed out of thin air")
+console.log("using `curl` we could get the raw content of a file from a Github repository")
+console.log("thats cool")
+console.log("right?!")
